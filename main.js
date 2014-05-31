74f9254b85a130c6a21b2424e6f35b783d12647a
david <david@kaustik.com>
2014-05-31 14:36:19 +0200

    funkar bra nu

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
+// Message Prototype
 var message = $$({
-  model: {},
-  view:{
-    format: 
-    '<li id="message"><div data-bind="from" /><div data-bind="to" /><div data-bind="message" /></li>'
-  }, 
-  controller: {}
-}).persist($$.adapter.restful, {collection: 'conversation', baseUrl: 'http://haikyou.herokuapp.com/'});
+	model: {},
+	view:{
+		format: '<li class="message-card fade in"><div data-bind="from" /><div data-bind="to" /><div data-bind="message" /><div data-bind="date" /></li>',
+	}, 
+	controller: {}
+}).persist($$.adapter.restful, {collection: 'conversation', baseUrl: 'http://localhost:3000/'});
 
-var conversation = $$({}, '<div>Conversation: <ul/></div').persist();
 
+// Conversation Container
+var conversation = $$({}, '<div><ul/></div', {
+	'persist:start': function(){
+	},
+
+	'persist:stop': function(){
+	}
+}).persist();
+
+
+// Where new messages are typed
+var messageBox = $$({}, '<div><form class="form" role="form"><div class="formgroup"><textarea id="message-text" /></div><div class="formgroup"><button type="button" id="new-message-btn" class="btn btn-primary">Send</button></div></form</div', {
+	'click button': function(){
+
+		var item = $$(message, {
+			controller: {
+				'persist:start':function(){
+				},
+
+				'persist:error':function(){
+					console.log('Could not save :(');
+				},
+
+				'persist:save:success':function(){
+					$$.document.prepend(this, '#conversation-container div ul');
+				}
+			}
+		});
+
+		item.model.set({'message':$('#message-text').val(), 'from':'Bebop', 'to':'Rocksteady'});
+		item.save();
+	},
+
+});
+
+$$.document.append(messageBox, '#new-message-container');
 $$.document.append(conversation, '#conversation-container');
 
 conversation.gather(message, 'append', 'ul');
